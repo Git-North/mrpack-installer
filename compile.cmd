f64b93f87541a802539288ce06675b83f64b099f
Git-North <72702125+Git-North@users.noreply.github.com>
2025-02-13 12:19:47 +0300

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -1,6 +1,6 @@
-for /f %%A in ('powershell -Command "(Invoke-WebRequest -Uri 'https://mvnrepository.com/artifact/org.json/json' -UseBasicParsing).Content -match 'Latest Version:\s*<a[^>]*>(.*?)</a>';$matches[1]"') do set "LATEST_VERSION=%%A"
-echo Latest json.jar version: %LATEST_VERSION%
-curl https://repo1.maven.org/maven2/org/json/json/%LATEST_VERSION%/json-%LATEST_VERSION%.jar -o json.jar
+for /f "delims=" %%a in ('powershell -command "(Invoke-WebRequest 'https://repo1.maven.org/maven2/org/json/json/maven-metadata.xml').Content | Select-String -Pattern '<latest>(.*?)</latest>' | ForEach-Object { $_.Matches.Groups[1].Value }"') do set latest=%%a
+echo Latest json.jar version: %latest%
+curl https://repo1.maven.org/maven2/org/json/json/%latest%/json-%latest%.jar -o json.jar
 echo Download complete.
 javac -cp "%~dp0json.jar;." mrpack_util.java
 pause
